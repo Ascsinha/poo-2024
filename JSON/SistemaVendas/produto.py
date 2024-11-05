5a381e5c6405e22ac0c63367ff26b729af485287
Acsa Mirella da Silva Dantas <20231041110004@ifrn.local>
2024-11-05 15:54:28 -0300

    Atividade 09 - Utilizando as bibliotecas padrão no nosso código Python

---
+++ JSON/SistemaVendas/produto.py
@@ -0,0 +1,37 @@
+from venda import Venda
+
+class Produto:
+    
+    def __init__(self, nome, preco, quantidade):
+        self.__nome = nome
+        self.__preco = preco
+        self.__quantidade = quantidade
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_preco(self):
+        return self.__preco
+
+    def get_quantidade(self):
+        return self.__quantidade
+
+    def set_nome(self, nome):
+        self.__nome = nome
+
+    def set_preco(self, preco):
+        self.__preco = preco
+
+    def set_quantidade(self, quantidade):
+        self.__quantidade = quantidade
+
+    def to_dict(self):
+        return {
+            "nome": self.__nome,
+            "preco": self.__preco,
+            "quantidade": self.__quantidade
+        }
+
+    @classmethod
+    def from_dict(cls, dados):
+        return cls(dados['nome'], Venda.from_dict(dados['preco']), dados['quantidade'])
